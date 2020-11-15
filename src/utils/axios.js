8e9d775bb6eb667a39eb0eb8e7854c6d57c4b679
sebbers <sebb.roeder@gmail.com>
2020-11-15 11:53:04 -0800

    Refresh token fix

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -10,6 +10,7 @@ instance.interceptors.request.use(
         return config;
     },
     error => {
+        console.log(error);
         Promise.reject(error);
     }
 );
@@ -20,49 +21,28 @@ instance.interceptors.response.use(
         return response;
     },
     error => {
-        debugger
         const { config: originalRequest } = error;
-        console.log(`originalRequest.url: ${originalRequest.url}`)
-        debugger
         if (error.response.status === 401 && originalRequest.url.endsWith('v1/admin')) {
-            debugger
             history.push('/login');
             return Promise.reject(error);
         }
 
         if (error.response.status === 401 && !originalRequest._retry) {
-            // const user = JSON.parse(localStorage.getItem('user'));
-            // const { refresh } = user || '';
-            // axios.defaults.headers.common['Authorization'] = `Bearer ${refresh}`;
-            // return instance(originalReq  uest);
-            debugger
             originalRequest._retry = true;
             const refreshToken = LocalStorage.getRefreshToken();
-            debugger
-            return axios.get('/api/v1/admin/',
+            return axios.get('https://license-test.digitalanarchy.com/api/v1/admin/refresh',
                 {
                     headers: {
                       'Authorization': `Bearer ${refreshToken}`
                     }
                 })
                 .then(response => {
-                    debugger
                     if (response.data) {
-                        debugger
-                        // localStorage.setItem("user", JSON.stringify(response.data));
                         LocalStorage.setToken(response.data);
-                        debugger
                         instance.defaults.headers.common['Authorization'] = `Bearer ${LocalStorage.getAccessToken()}`;
-                        debugger
-                        // return response.data.token;
                         return instance(originalRequest);
                       }
 
-                    // if (res.status === 201) {
-                    //     localStorageService.setToken(res.data);
-                    //     axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorageService.getAccessToken();
-                    //     return axios(originalRequest);
-                    // }
                 })
         }
         return Promise.reject(error);
